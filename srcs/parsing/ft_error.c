e09ce1b80e156eb208e2b0bc40249fa0a165fd17
Aymeric Jakubczyk <ajakubcz@z4r8p3.42lyon.fr>
2023-07-27 15:31:16 +0200

    resolve some little problems

--- srcs/parsing/ft_error.c
+++ srcs/parsing/ft_error.c
@@ -18,7 +18,34 @@ void	ft_error(const char *error, char *problem, int nbrerror)
 	if (nbrerror)
 		g_errno = nbrerror;
 	if (problem != NULL)
-		ft_dprintf("(s)Hell : %s %s\n", problem, error);
+	{
+		ft_print_error("(s)Hell : ", error, problem);
+		// ft_dprintf("(s)Hell : %s %s\n", problem, error);
+	}
 	else
-		ft_dprintf("(s)Hell : %s\n", error);
+	{
+		ft_print_error("(s)Hell : ", error, NULL);
+		// ft_dprintf("(s)Hell : %s\n", error);
+	}
+}
+
+void	ft_print_error(char *prompt, const char *error, char *problem)
+{
+	char *str;
+
+	str = ft_strdup("");
+	str = ft_strjoin3(str, prompt);
+	if (problem)
+	{
+		str = ft_strjoin3(str, " ");
+		str = ft_strjoin3(str, problem);
+	}
+	if (error)
+	{
+		str = ft_strjoin3(str, " ");
+		str = ft_strjoin3(str, (char *)error);
+	}
+	str = ft_strjoin3(str, "\n");
+	write(2, str, ft_strlen(str));
+	free(str);
 }
